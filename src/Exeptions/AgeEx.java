2ec553f4f0011e42a0c84ceb4d8e89e929c97b2b
toktobek-01 <toktobek.d@gmail.com>
2025-06-27 15:42:45 -0500

    new

--- src/Exeptions/AgeEx.java
+++ src/Exeptions/AgeEx.java
@@ -1,5 +1,7 @@
 package Exeptions;
 
+import java.util.Scanner;
+
 public class AgeEx {
     static void printHello(int age, String name) throws Exception {
         if (age < 18) {
@@ -8,14 +10,29 @@ public class AgeEx {
         if (name.isEmpty()) {
             throw new IllegalArgumentException("Name cannot be empty...");
         }
-        System.out.println("Hello" + name + "!");
+        System.out.println("Hello, " + name + "!");
     }
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         try {
-            printHello(13, "John");
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
+            System.out.println("Enter your name: ");
+            String name = scanner.nextLine();
+
+            System.out.println("Enter your age: ");
+            String ageInput = scanner.nextLine();
+            int age = Integer.parseInt(ageInput);
+            printHello(age, name);
+
+        } catch (NumberFormatException e) {
+            System.out.println("Please enter a valid number for age.");
+        } catch (IllegalArgumentException e ) {
+            System.out.println(" ! " + e.getMessage());
+        } catch (Exception e ) {
+            System.out.println(" ! " + e.getMessage());
+        } finally {
+            scanner.close();
+            System.out.println("Successful!!!");
         }
     }
 }
